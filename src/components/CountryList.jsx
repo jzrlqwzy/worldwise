b5a371d51e4a61b26424643d35bc7f9bce502eb4
lei <littlesomerest@gmail.com>
2024-05-21 10:56:35 +0800

    country list

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,29 @@
+import CountryItem from "./CountryItem";
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking a city on the map" />
+    );
+
+  const countries = cities.reduce((acc, cur) => {
+    if (!acc.map((el) => el.country).includes(cur.country))
+      return [...acc, { country: cur.country, emoji: cur.emoji }];
+    else return acc;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={countries.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
