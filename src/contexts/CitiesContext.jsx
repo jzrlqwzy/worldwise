040ed614de9ab175a2029b2b8f411b8e74292c48
lei <littlesomerest@gmail.com>
2024-05-23 11:47:21 +0800

    finish useReducer replace useState

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -31,6 +31,7 @@ const reducer = function (state, action) {
         ...state,
         isLoading: false,
         cities: [...state.cities, action.payload],
+        currentCity: action.payload,
       };
 
     case "city/deleted":
@@ -38,6 +39,7 @@ const reducer = function (state, action) {
         ...state,
         isLoading: false,
         cities: state.cities.filter((city) => city.id !== action.payload),
+        currentCity: {},
       };
 
     case "rejected":
@@ -49,7 +51,7 @@ const reducer = function (state, action) {
 };
 
 function CitiesProvider({ children }) {
-  const [{ cities, isLoading, currentCity }, dispatch] = useReducer(
+  const [{ cities, isLoading, currentCity, error }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -70,6 +72,8 @@ function CitiesProvider({ children }) {
   }, []);
 
   async function getCity(id) {
+    if (Number(id) === currentCity.id) return;
+
     dispatch({ type: "loading" });
 
     try {
@@ -118,6 +122,7 @@ function CitiesProvider({ children }) {
         cities,
         isLoading,
         currentCity,
+        error,
         getCity,
         createCity,
         deleteCity,
